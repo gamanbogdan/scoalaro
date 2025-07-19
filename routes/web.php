9a916749f33847565de0b879e5357cf32cd5f198
unknown <gaman.anamaria@gmail.com>
2025-07-19 22:07:05 +0300

    Cel de-al doilea  commit al aplicației Laravel scoalaro

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::middleware('api')->prefix('api')->group(function () {
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', fn (Request $request) => $request->user());
+    });
+
 });
